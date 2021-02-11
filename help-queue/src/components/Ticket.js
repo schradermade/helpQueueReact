7f5742cbc70c140e78eaa4685013076dcde0e554
Nathan Schrader <nathanschrader@icloud.com>
2021-02-11 10:54:07 -0800

    add PropTypes to Ticket

---
+++ help-queue/src/components/Ticket.js
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Ticket(props) {
+  const name = "Thato";
+  const name2 = "Haley";
+  return (
+    <React.Fragment>
+      <h3>3a</h3>
+      <h3>{props.location} and {props.names}</h3>
+      <p><em>{props.issue}</em></p>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+Ticket.propTypes = {
+  names: PropTypes.string,
+  location: PropTypes.string,
+  issue: PropTypes.string
+};
+
+export default Ticket;
